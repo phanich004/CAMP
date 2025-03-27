364187bbc65cdf63fbaa7491907ddb31c93f74c6
PHANENDER CHALASANI <phanenderchalasani@gmail.com>
2025-03-27 00:42:09 -0500

    Initial Commit

---
+++ CAMSS/CAMSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CAMSSApp.swift
+//  CAMSS
+//
+//  Created by Phanender Chalasani on 27/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CAMSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
